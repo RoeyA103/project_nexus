f4c8e22a4390162efd6bcecde3a8413a18ac2e0e
daniel <ddny3622@gmail.com>
2026-03-25 13:03:54 +0200

    add

--- web-server/frontend/vite.config.js
+++ web-server/frontend/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite";  
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true, 
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "http://backend-api:4000", 
         changeOrigin: true,
-        credentials: true,
       },
     },
   },
